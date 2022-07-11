f74db5a9d30e5d1243e6d9edc528a8b7b9b570ff
karimy <karim.alrashdan14@gmail.com>
2022-07-11 07:10:09 -0600

    Practiced console logs

--- variables.js
+++ variables.js
@@ -3,8 +3,15 @@ var name = "Karim Al-Rashdan"
 var turingCourse = "Front End"
 var age = 24
 var birthYear = 1998
-var tuitionCost = 20,000
+var tuitionCost = 20000
 var depositPaid = true
 var turingEnrolled = true
 var graduatedTuring = false
-var sentence = `{name} student roster`;// students enrolled in Turing Class of 2023
+var sentence = `{name} student roster`;// students enrolled in Turing Course
+email = "karim.alrashdan@gmail.com"
+name = "Karim Hazel"
+turingCourse = "Back End"
+console.log (email)
+console.log (name)
+console.log (turingCourse)
+console.log (turingEnrolled)
